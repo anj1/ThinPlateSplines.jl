523d94b4be6d15e88bcee5a1aacd5ab3949cc5da
RainerHeintzmann <heintzmann@gmail.com>
2023-03-27 18:18:43 +0200

    improved performance, added docs and tests

---
+++ test/runtests.jl
@@ -0,0 +1,46 @@
+using Test
+using ThinPlateSplines
+
+@testset "tps generation" begin
+    x1 = [0.0 1.0 
+    1.0 0.0
+    1.0 1.0]
+    x2 = [0.0 1.0
+      1.1 0.0
+      1.2 1.5]
+    tps = tps_solve(x1, x2, 1.0)
+    @test tps.Y == [ 1.0  0.0  1.0
+    1.0  1.1  0.0
+    1.0  1.2  1.5]
+    @test tps.c == zeros((3,3))
+    @test tps.d ≈ [  1.0          -0.1  -0.5
+    0   1.2   0.5
+    0   0.1   1.5]
+    @test isapprox(tps.Φ, [ 0.0       0.693147  0.0
+    0.693147  0.0       0.0
+    0.0       0.0       0.0], atol=1e-5)
+end
+
+x1 = [0.0 1.0 
+1.0 0.0
+1.0 1.0]
+x2 = [0.0 1.0
+  1.1 0.0
+  1.2 1.5]
+tps = tps_solve(x1, x2, 1.0)
+
+@testset "tps_deform" begin
+    
+    x = [1.0 0.0
+     2.0 2.0]
+    y = tps_deform(x,tps)
+    @test y ≈ [ 1.1  0
+    2.5   3.5]
+    y = tps_deform(x1, x, x2, 1.0)
+    @test y ≈ [ 1.1  0
+    2.5   3.5]
+end
+
+@testset "tps_energy" begin
+    @test tps_energy(tps) ≈ 0
+end
